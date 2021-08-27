d99708caa6990ef3683a9e371c564aa298e92d0e
Austin Bailey <Sbailea795@outlook.com>
2021-08-27 01:09:01 -0500

    Added scytale

--- atbash.c
+++ atbash.c
@@ -8,19 +8,19 @@
 #define tebahpla "zyxwvutsrqponmlkjihgfedcba"
 int main(int argc, char **argv)
 {
-    if(argc > 1)
+    if (argc > 1)
     {
         for (int args = 1; args < argc; args++)
         {
             char *argPtr = argv[args];
             for (int i = 0; i < strlen(argPtr); i++)
             {
-                if(argPtr[i] >= 0x41 && argPtr[i] <= 0x122)
+                if (argPtr[i] >= 0x41 && argPtr[i] <= 0x122)
                 {
                     argPtr[i] = 122 - tolower(argPtr[i]) + 97;
                 }
-        }
-        printf("%s\n", argPtr);
+            }
+            printf("%s\n", argPtr);
         }
         return EXIT_SUCCESS;
     }
---
+++ scytale.c
@@ -0,0 +1,79 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+int usage(void);
+
+int main(int argc, char *argv[])
+{
+  int opt;
+  int counter = 0;
+  char *charPtr;
+  bool readCypher = false;
+  int argStart = 3;
+  int polygonalSize;
+  char *argPtr;
+  if (strcmp(argv[1], "-s") != 0)
+    usage();
+  if (strcmp(argv[3], "-r") == 0)
+  {
+    argStart++;
+    readCypher++;
+  }
+  polygonalSize = strtol(argv[2], &charPtr, 10);
+  if (*charPtr != 0)
+    usage();
+  argPtr = argv[argStart];
+  if (!readCypher)
+  {
+    for (int args = argStart; args < argc; args++)
+    {
+      argPtr = argv[argStart];
+      char *scytale;
+      scytale = (char *)malloc(512 * sizeof(char));
+      scytale[0] = '\0';
+      for (int size = 0; size < polygonalSize; size++)
+      {
+        for (int index = size; index < strlen(argPtr); index += polygonalSize)
+        {
+          *(scytale + counter++) = argPtr[index];
+          printf("%c\n", argPtr[index]);
+        }
+      }
+      *(scytale + counter) = '\0';
+      printf("%s\n", scytale);
+    }
+  }
+  else
+  {
+    for (int args = argStart; args < argc; args++)
+    {
+      argPtr = argv[argStart];
+      char *scytale;
+      scytale = (char *)malloc(512 * sizeof(char));
+      scytale[0] = '\0';
+      int passes = ceil( (double) strlen(argPtr) / polygonalSize);
+      for (int size = 0; size <= polygonalSize; size++)
+      {
+        for (int index = size; index < strlen(argPtr); index += passes)
+        {
+          *(scytale + counter++) = argPtr[index];
+        }
+      }
+      *(scytale + counter) = '\0';
+      printf("%s\n", scytale);
+    }
+  }
+  return EXIT_SUCCESS;
+}
+
+int usage()
+{
+  printf("Usage: scytale -s <Size> [-r] <Text1> [Text2]...\n\n");
+  printf("  -s: size - sets the gap between letters. functionally -s denotes the sides of a polygon, wherein scytale reads across the first face.\n");
+  printf("  -r: reverse - reads scytale with the key of <Size>\n\n");
+  exit(EXIT_FAILURE);
+}
